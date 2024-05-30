945ffda0eac5c746766a3720312e8c6c6ed121da
Maya Ouazene <2247904@bdeb.qc.ca>
2024-05-30 18:38:05 -0400

    tentative d'aujout d'un carousel d'image

---
+++ script.js
@@ -0,0 +1,35 @@
+let currentIndex = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.carousel-images img');
+    const totalSlides = slides.length;
+
+    if (index >= totalSlides) {
+        currentIndex = 0;
+    } else if (index < 0) {
+        currentIndex = totalSlides - 1;
+    } else {
+        currentIndex = index;
+    }
+
+    const newTransform = `translateX(-${currentIndex * 100/ totalSlides}%)`;
+    document.querySelector('.carousel-images').style.transform = newTransform;
+}
+
+function nextSlide() {
+    showSlide(currentIndex + 1);
+}
+
+function prevSlide() {
+    showSlide(currentIndex - 1);
+}
+
+function autoSlide() {
+    nextSlide();
+    setTimeout(autoSlide, 5000); 
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    showSlide(currentIndex);
+    setTimeout(autoSlide, 5000);
+});
